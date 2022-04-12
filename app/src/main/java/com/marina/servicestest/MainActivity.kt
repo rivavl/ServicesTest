f0b26d8a7b903e6f40169530fbaf7d0f1fb6b921
rivavl <mvkalinenkova@yandex.ru>
2022-04-12 13:56:42 +0300

    init

---
+++ app/src/main/java/com/marina/servicestest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.marina.servicestest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marina.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
